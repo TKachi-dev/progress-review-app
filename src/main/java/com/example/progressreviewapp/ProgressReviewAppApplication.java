99f18dfd43f45ea5f25f78b0826fc9e7d6af9089
Takuma Kachi <tk1205.626@gmail.com>
2026-04-15 23:26:20 +0900

    新規作成

---
+++ src/main/java/com/example/progressreviewapp/ProgressReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.progressreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressReviewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgressReviewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/progressreviewapp/ProgressReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progressreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressReviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
