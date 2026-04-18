28a9ded6c390dd28639e744ed86df5614131be15
Takuma Kachi <tk1205.626@gmail.com>
2026-04-18 18:06:49 +0900

    Entity: ProgressRecord作成

---
+++ src/main/java/com/example/progressreviewapp/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.progressreviewapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/test")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/main/java/com/example/progressreviewapp/entity/ProgressRecord.java
@@ -0,0 +1,53 @@
+package com.example.progressreviewapp.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "progress_record")
+public class ProgressRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate date;
+    private String didWhat;
+    private String nextAction;
+    private String blockedPoint;
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDidWhat() {
+        return didWhat;
+    }
+
+    public void setDidWhat(String didWhat) {
+        this.didWhat = didWhat;
+    }
+
+    public String getNextAction() {
+        return nextAction;
+    }
+
+    public void setNextAction(String nextAction) {
+        this.nextAction = nextAction;
+    }
+
+    public String getBlockedPoint() {
+        return blockedPoint;
+    }
+
+    public void setBlockedPoint(String blockedPoint) {
+        this.blockedPoint = blockedPoint;
+    }
+}
